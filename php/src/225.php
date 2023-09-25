6779f274d8cf5c6b2be904093a2094b7e9f85c1b
Reon710 <rubochnit@alu.edu.gva.es>
2023-09-25 18:11:40 +0200

    [MOD]

--- php/src/225.php
+++ php/src/225.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <?php
-    extract($_POST);
-    const ABECEDARI = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+extract($_POST);
+const ABECEDARI = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 ?>
 
 <head>
@@ -14,18 +14,18 @@
 <body>
 <table>
     <?php
-        if (isset($files,$columnes)) {
-            for ($i = 0; $i < $files; $i++) {
-                $letraColumna = ABECEDARI[$i];
-                echo "<tr> ";
-                for ($j = 1; $j <= $columnes; $j++) {
-                    echo "<td>${letraColumna}$j</td> ";
-                }
-                echo "</tr> ";
+    if (isset($files,$columnes)) {
+        for ($i = 0; $i < $files; $i++) {
+            $letraColumna = ABECEDARI[$i];
+            echo "<tr> ";
+            for ($j = 1; $j <= $columnes; $j++) {
+                echo "<td>${letraColumna}$j</td> ";
             }
-        } else {
-            echo "Posa el valor de le files i les columnes en el QueryString";
+            echo "</tr> ";
         }
+    } else {
+        echo "Posa el valor de le files i les columnes en el QueryString";
+    }
     ?>
 </table>
 </body>
